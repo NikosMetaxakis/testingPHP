62fe3e0c4495ca29ddac5bc22edaa61830979659
Nikos Metaxakis <metaxakisn@gmail.com>
2020-10-14 16:56:12 +0300

    retention of values in checkbox, radio buttons, select and select multiple on error or resubmission

--- form.php
+++ form.php
@@ -77,26 +77,66 @@
     ?>"><br>
     Password: <input type="password" name="password"><br>
     Gender:
-        <input type="radio" name="gender" value="f">female
-        <input type="radio" name="gender" value="m">male
-        <input type="radio" name="gender" value="o">other <br>
+        <input type="radio" name="gender" value="f" <?php
+            if($gender === 'f'){
+                echo 'checked';
+            }
+        ?>>female
+        <input type="radio" name="gender" value="m" <?php
+            if($gender === 'm'){
+                echo 'checked';
+            }
+        ?>>male
+        <input type="radio" name="gender" value="o" <?php
+            if($gender === 'o'){
+                echo 'checked';
+            }
+        ?>>other <br>
     Favorite color:
         <select name="color">
             <option value="">Please Select</option>
-            <option value="#f00">red</option>
-            <option value="#0f0">green</option>
-            <option value="00f">blue</option>
+            <option value="#f00" <?php
+                if($color === '#f00'){
+                    echo 'selected';
+                }
+            ?>>red</option>
+            <option value="#0f0" <?php
+                if($color === '#0f0'){
+                    echo 'selected';
+                }
+            ?>>green</option>
+            <option value="00f" <?php
+                if($color === '#00f'){
+                    echo 'selected';
+                }
+            ?>>blue</option>
         </select><br>
     languages spoken:
         <select name="languages[]" multiple size="3">
-            <option value="en">English</option>
-            <option value="fr">French</option>
-            <option value="it">Italian</option>
+            <option value="en" <?php
+                if (in_array('en', $languages)){
+                    echo 'selected';
+                }
+            ?>>English</option>
+            <option value="fr" <?php
+                if (in_array('fr', $languages)){
+                    echo 'selected';
+                }
+            ?>>French</option>
+            <option value="it" <?php
+                if (in_array('it', $languages)){
+                    echo 'selected';
+                }
+            ?>>Italian</option>
         </select><br>
     Comments: <textarea name="comments"><?php
         echo htmlspecialchars($comments, ENT_QUOTES);
     ?></textarea><br>
-    <input type="checkbox" name="tc" value="ok">
+    <input type="checkbox" name="tc" value="ok" <?php
+        if($tc === 'ok'){
+            echo 'checked';
+        }
+    ?>>
         I accept the T&amp;C<br>
     <input type="submit" name="submit" value="Register">
 </form>
