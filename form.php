179741b87ae8d245990dead33ca3020dbba76bf4
Nikos Metaxakis <metaxakisn@gmail.com>
2020-10-14 18:27:02 +0300

    modified form + use of a mysql database

--- form.php
+++ form.php
@@ -1,11 +1,11 @@
 <?php
     $name = '';
-    $password = '';
+    //$password = '';
     $gender = '';
     $color = '';
-    $languages = [];
-    $comments = '';
-    $tc = '';
+    //$languages = [];
+    //$comments = '';
+    //$tc = '';
 
     if (isset($_POST['submit'])){
         $ok = true;
@@ -15,11 +15,11 @@
         }else{
             $name = $_POST['name'];
         }
-        if(!isset($_POST['password']) || $_POST['password'] === '') {
+        /* if(!isset($_POST['password']) || $_POST['password'] === '') {
             $ok = false;
         }else{
             $password = $_POST['password'];
-        }
+        } */
         if(!isset($_POST['gender']) || $_POST['gender'] === '') {
             $ok = false;
         }else{
@@ -30,38 +30,40 @@
         }else{
             $color = $_POST['color'];
         }
-        if(!isset($_POST['languages']) || !is_array($_POST['languages']) || count($_POST['languages']) === 0) {
+        /* if(!isset($_POST['languages']) || !is_array($_POST['languages']) || count($_POST['languages']) === 0) {
             $ok = false;
         }else{
             $languages = $_POST['languages'];
-        }
-        if(!isset($_POST['comments']) || $_POST['comments'] === '') {
+        } */
+        /* if(!isset($_POST['comments']) || $_POST['comments'] === '') {
             $ok = false;
         }else{
             $comments = $_POST['comments'];
-        }
-        if(!isset($_POST['tc']) || $_POST['tc'] === '') {
+        } */
+        /* if(!isset($_POST['tc']) || $_POST['tc'] === '') {
             $ok = false;
         }else{
             $tc = $_POST['tc'];
-        }
+        } */
         
         if($ok) {
-            printf("User name: %s
-            <br>Password: %s
-            <br>Gender: %s
-            <br>Color: %s
-            <br>Language(s): %s
-            <br>Comments: %s
-            <br>T&amp;C: %s",
-            htmlspecialchars($name, ENT_QUOTES),
-            htmlspecialchars($password, ENT_QUOTES),
-            htmlspecialchars($gender, ENT_QUOTES),
-            htmlspecialchars($color, ENT_QUOTES),
-            htmlspecialchars(implode(' ', $languages),  ENT_QUOTES),
-            htmlspecialchars($comments, ENT_QUOTES),
-            htmlspecialchars($tc, ENT_QUOTES)
+            //add database code here
+            $db = new mysqli(
+                'localhost',
+                'root',
+                '',
+                'php'
             );
+            $sql = sprintf(
+                "INSERT INTO users (name, gender, color) VALUES (
+                    '%s', '%s', '%s')",
+                $db->real_escape_string($name),
+                $db->real_escape_string($gender),
+                $db->real_escape_string($color)
+            );
+            $db->query($sql);
+            echo '<p>User added.</p>';
+            $db->close();
         }
     }
 ?>
@@ -75,7 +77,6 @@
     User name: <input type="text" name="name" value="<?php
       echo htmlspecialchars($name, ENT_QUOTES);  
     ?>"><br>
-    Password: <input type="password" name="password"><br>
     Gender:
         <input type="radio" name="gender" value="f" <?php
             if($gender === 'f'){
@@ -111,33 +112,6 @@
                 }
             ?>>blue</option>
         </select><br>
-    languages spoken:
-        <select name="languages[]" multiple size="3">
-            <option value="en" <?php
-                if (in_array('en', $languages)){
-                    echo 'selected';
-                }
-            ?>>English</option>
-            <option value="fr" <?php
-                if (in_array('fr', $languages)){
-                    echo 'selected';
-                }
-            ?>>French</option>
-            <option value="it" <?php
-                if (in_array('it', $languages)){
-                    echo 'selected';
-                }
-            ?>>Italian</option>
-        </select><br>
-    Comments: <textarea name="comments"><?php
-        echo htmlspecialchars($comments, ENT_QUOTES);
-    ?></textarea><br>
-    <input type="checkbox" name="tc" value="ok" <?php
-        if($tc === 'ok'){
-            echo 'checked';
-        }
-    ?>>
-        I accept the T&amp;C<br>
     <input type="submit" name="submit" value="Register">
 </form>
 
